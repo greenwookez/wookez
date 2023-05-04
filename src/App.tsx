ed620b7ae0a39a9dcd0ee7966e823f77c62b5670
Bogdan Bogdanov <greenwookez@gmail.com>
2023-05-04 15:58:21 +0300

    add sample page

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,35 @@
+import { CreateSheet, css } from "packages/aphrodite"
+
 const App: React.FC = () => {
+  const onContactClick = () => {
+    const a = document.createElement("a")
+    a.href = "mailto:contact@wookez.com"
+    a.click()
+  }
+
   return (
-    <>
-      Hello world
-    </>
+    <div className={css(Styles.container)}>
+      <div className={css(Styles.content)}>
+        <img src='/modern-problems-chapelle.gif' />
+        <button onClick={onContactClick}>Text me</button>
+      </div>
+    </div>
   )
 }
 
 export default App
+
+const Styles = CreateSheet({
+  container: {
+    minWidth: "100vw",
+    minHeight: "100vh",
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  content: {
+    display: "flex",
+    flexDirection: "column",
+    gap: 20,
+  },
+})
